7ded15df288b66336d4e6d989c567efcaacad959
Truong Nguyen <ntt261298@gmail.com>
2021-03-23 22:31:25 +0700

    feat: Add auth utils

---
+++ packages/commons/src/utils/auth.js
@@ -0,0 +1,30 @@
+export default class Auth {
+  constructor(storage, authKey = 'authentication') {
+    this.storage = storage;
+    this.authKey = authKey;
+    this.data = this.storage.getJson(this.authKey);
+    if (this.data) {
+      this.storage.setCookie('Authorization', `Bearer ${this.data.accessToken}`);
+    }
+  }
+
+  getToken() {
+    return this.data?.accessToken;
+  }
+
+  isAuth() {
+    return !!(this.getToken())
+  }
+
+  setAuth(data) {
+    this.data = data;
+    this.storage.setJson(this.authKey, data);
+    this.storage.setCookie('Authorization', `Bearer ${this.data?.accessToken}`)
+  }
+
+  logout() {
+    this.data = {};
+    this.storage.removeItem(this.authKey);
+    this.storage.removeCookie('Authorization');
+  }
+}
